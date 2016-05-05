7eee4e4840d96981aed17ce8f2717b468add75fe
bondarmih <bondarmih@gmail.com>
2016-05-05 09:26:00 +0400

    lection 2 random array generator added

---
+++ lection#2Objects.and.Classes/objectsandclasses/src/bondarmih/edu/genetator/RandomArrayGenerator.java
@@ -0,0 +1,26 @@
+package bondarmih.edu.genetator;
+
+/**
+ *
+ * Created by bondarm on 05.05.16.
+ */
+public class RandomArrayGenerator {
+    public static double[] generateArray(int length) {
+        double module = 100;
+        double[] generatedArray = new double[length];
+        for (int i=0; i<length; i++) {
+            generatedArray[i] = Math.random()*module*2-module;
+        }
+        return generatedArray;
+    }
+
+    public static double[] generateArray() {
+        double module = 100;
+        int length = (int)(Math.random()*100);
+        double[] generatedArray = new double[length];
+        for (int i=0; i<length; i++) {
+            generatedArray[i] = Math.random()*module*2-module;
+        }
+        return generatedArray;
+    }
+}
--- lection#2Objects.and.Classes/objectsandclasses/src/bondarmih/edu/main/SorterTest.java
+++ lection#2Objects.and.Classes/objectsandclasses/src/bondarmih/edu/main/SorterTest.java
@@ -1,5 +1,6 @@
 package bondarmih.edu.main;
 
+import bondarmih.edu.genetator.RandomArrayGenerator;
 import bondarmih.edu.sorters.CocktailSorter;
 import bondarmih.edu.sorters.SelectionSorter;
 import bondarmih.edu.sorters.Sorter;
@@ -13,10 +14,11 @@ import java.util.Arrays;
 public class SorterTest{
 
     private static void testSorterInstance(Sorter testedInstance) {
-        double[] initialArray = {2.0,4.0,4.0,6.0,4.0,6.0,7.0,36.0,6.0,4.0,4.0,4.0,43.0,5.0,6.0,7.0,4.0,3.0,5.0,5.0,3.0};
+        //double[] initialArray = {2.0,4.0,4.0,6.0,4.0,6.0,7.0,36.0,6.0,4.0,4.0,4.0,43.0,5.0,6.0,7.0,4.0,3.0,5.0,5.0,3.0};
+        double[] initialArray = RandomArrayGenerator.generateArray(100);
         System.out.println("Sort and search test");
         System.out.println(testedInstance.sorterDescription());
-        double itemToSearch = 4.0;
+        double itemToSearch = initialArray[14];
         int position;
 
         System.out.println("Initial array is: "+ Arrays.toString(initialArray));
