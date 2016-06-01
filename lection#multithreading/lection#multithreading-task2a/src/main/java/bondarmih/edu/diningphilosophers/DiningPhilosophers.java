ba4f4c28af4dc10474c21f008ea568076fc7c61b
bondarmih <bondarmih@gmail.com>
2016-06-01 08:29:09 +0400

    lection#multithreading - dining philosophers - task a

---
+++ lection#multithreading/lection#multithreading-task2a/src/main/java/bondarmih/edu/diningphilosophers/DiningPhilosophers.java
@@ -0,0 +1,49 @@
+package bondarmih.edu.diningphilosophers;
+
+/**
+ * Created by bondarm on 31.05.16.
+ */
+public class DiningPhilosophers {
+
+    private static boolean inDeadlock = false;
+
+    public static void run() {
+
+        Fork[] forks = new Fork[5];
+        for (int i = 0; i < forks.length; i++) {
+            forks[i] = new Fork(i);
+        }
+        Philosopher[] philosophers = new Philosopher[5];
+        for (int i = 0; i < philosophers.length; i++) {
+            philosophers[i] = new Philosopher(i,forks[i], forks[((i+1) % 5)]);
+            Thread ph = new Thread(philosophers[i]);
+            ph.start();
+        }
+
+        while (!Thread.interrupted()) {
+            try {
+                Thread.sleep(100);
+                if (!inDeadlock && deadlockMark(forks)) {
+                    System.out.println("Deadlock!");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static boolean deadlockMark(Fork[] forks) {
+        boolean result = true;
+        int i = 0;
+        while (result == true && i < forks.length) {
+            if (forks[i].getHolder() == null) {
+                return false;
+            } else {
+                result = result && (forks[i].getHolder().getId() == i);
+            }
+            i++;
+        }
+        inDeadlock = result;
+        return result;
+    }
+}
---
+++ lection#multithreading/lection#multithreading-task2a/src/main/java/bondarmih/edu/diningphilosophers/PhilosopherState.java
@@ -0,0 +1,6 @@
+package bondarmih.edu.diningphilosophers;
+
+/**
+ * Created by bondarm on 31.05.16.
+ */
+public enum PhilosopherState {EATING,THINKING,HUNGRY};
